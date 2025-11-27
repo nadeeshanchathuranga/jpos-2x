b0b2f6abdb68e8ea5b96eba29bae2f386dd4ecdf
MenalinR <142731196+MenalinR@users.noreply.github.com>
2025-11-27 11:52:02 +0530

    removed measurement unit field

--- resources/js/Pages/Products/Components/ProductCreateModal.vue
+++ resources/js/Pages/Products/Components/ProductCreateModal.vue
@@ -235,19 +235,7 @@
               <span class="text-xs text-gray-400">Alert when stock falls below this level</span>
             </div>
 
-            <!-- Measurement Unit -->
-            <div>
-              <label class="block mb-2 text-sm font-medium text-white">Base Measurement Unit</label>
-              <select
-                v-model="form.measurement_unit_id"
-                class="w-full px-4 py-2 text-white bg-gray-800 border border-gray-700 rounded focus:outline-none focus:border-blue-500"
-              >
-                <option value="">Select Unit</option>
-                <option v-for="unit in measurementUnits" :key="unit.id" :value="unit.id">
-                  {{ unit.name }}
-                </option>
-              </select>
-            </div>
+            
 
             <!-- Purchase Unit -->
             <div>
