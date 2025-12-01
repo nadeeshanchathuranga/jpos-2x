8dd6a75e59e34fefae78c4a340f830c44c6aefcf
MenalinR <142731196+MenalinR@users.noreply.github.com>
2025-12-01 11:25:48 +0530

    created popup to add expenses

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -6,5 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class Expense extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'amount',
+        'remark',
+        'expense_date',
+        'payment_type',
+        'user_id',
+    ];
+
+    protected $casts = [
+        'expense_date' => 'date',
+        'amount' => 'decimal:15,2',
+        'payment_type' => 'integer',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getPaymentTypeNameAttribute()
+    {
+        $types = [
+            0 => 'Cash',
+            1 => 'Card',
+            2 => 'Credit',
+            3 => 'Cheque',
+        ];
+        return $types[$this->payment_type] ?? 'Unknown';
+    }
 }
