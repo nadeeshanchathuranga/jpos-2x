584c1b6c9aba2799b5360c3f0b9e8d5bce610aae
Nadishan Chathuranga <nadishanchathuranga21@gmail.com>
2025-12-16 11:18:44 +0530

    table fix user table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+             $table->tinyInteger('role')->default(0)->comment('0=Admin, 1=Manager, 2=Cashier, 3=Salesmen');
             $table->rememberToken();
             $table->timestamps();
         });
