2baf83b51cce552dcf6c72133f5ad1943611d005
ujithe <ujithegodawaththa@gmail.com>
2025-12-18 12:47:10 +0530

    layout changes in login page

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -4,14 +4,8 @@ import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <div
-        class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0"
-    >
-         
-
-        <div
-            class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md    sm:rounded-lg"
-        >
+    <div>
+        <div>
             <slot />
         </div>
     </div>
